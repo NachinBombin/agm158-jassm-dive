ecd47cd2cc27b00b0755746253adc32eabb49bee
NachinBombin <solidnachox@gmail.com>
2026-04-23 00:52:31 -0400

    Fix invisible missile: restore ENT.Base=base_entity and ENT.RenderGroup=RENDERGROUP_OPAQUE in shared scope

--- lua/entities/ent_bombin_jassm/shared.lua
+++ lua/entities/ent_bombin_jassm/shared.lua
@@ -1,5 +1,5 @@
 ENT.Type           = "anim"
-ENT.Base           = "base_anim"
+ENT.Base           = "base_entity"
 ENT.PrintName      = "Bombin JASSM"
 ENT.Author         = "NachinBombin"
 ENT.Information    = "AGM-158 JASSM loitering-mode missile. 3D model flame trail, full AN-71 positional audio."
@@ -7,3 +7,5 @@ ENT.Category       = "Bombin Support"
 
 ENT.Spawnable      = false
 ENT.AdminSpawnable = false
+
+ENT.RenderGroup    = RENDERGROUP_OPAQUE
