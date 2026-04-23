fa1ea8d671b62cbafada82b9e6bcfcb017bbcc40
NachinBombin <solidnachox@gmail.com>
2026-04-22 23:36:58 -0400

    Fix invisible missile: change ENT.Base from base_entity to base_anim

--- lua/entities/ent_bombin_jassm/shared.lua
+++ lua/entities/ent_bombin_jassm/shared.lua
@@ -1,5 +1,5 @@
 ENT.Type           = "anim"
-ENT.Base           = "base_entity"
+ENT.Base           = "base_anim"
 ENT.PrintName      = "Bombin JASSM"
 ENT.Author         = "NachinBombin"
 ENT.Information    = "AGM-158 JASSM loitering-mode missile. 3D model flame trail, full AN-71 positional audio."
@@ -7,8 +7,3 @@ ENT.Category       = "Bombin Support"
 
 ENT.Spawnable      = false
 ENT.AdminSpawnable = false
-
--- RENDERGROUP_OPAQUE is client-only; guard it so shared.lua doesn't crash on the server
-if CLIENT then
-	ENT.RenderGroup = RENDERGROUP_OPAQUE
-end
